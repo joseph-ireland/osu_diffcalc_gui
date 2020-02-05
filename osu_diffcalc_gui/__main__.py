10d8e6c1e955e7f566b58655f7cc69ad1b77a037
Joseph Ireland <joseph.ireland@outlook.com>
2020-02-05 23:17:27 +0000

    fix --help

--- osu_diffcalc_gui/__main__.py
+++ osu_diffcalc_gui/__main__.py
@@ -6,10 +6,10 @@ from PySide2 import QtWidgets
 from osu_diffcalc_gui.mainwindow import MainWindow
 
 def main():
-    parser = argparse.ArgumentParser("osu_test_map_gen")
+    parser = argparse.ArgumentParser("python -m osu_diffcalc_gui")
     parser.add_argument("-c","--command_prefix",help="prepended to command before executing")
     parser.add_argument("-d","--directory", help="Working directory of command")
-    parser.add_argument("-f", "--file", help="Name of output .osu file", default="test.osu")
+    parser.add_argument("-f", "--file", help="Name of output .osu file(default='test.osu')", default="test.osu")
     args = parser.parse_args()
 
     app = QtWidgets.QApplication(sys.argv)
